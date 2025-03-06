662323583f25ef38af6a964df9df05b963cf53e8
Kien Le <kien.le2152004@hcmut.edu.vn>
2025-03-06 16:08:58 +0700

    change model

--- spell_checking.py
+++ spell_checking.py
@@ -5,7 +5,11 @@ corrector = pipeline(
               'pszemraj/flan-t5-large-grammar-synthesis',
               )
 
-
+def correct_text(text):
+    prompt = f"Fix grammar and spelling: {text}"
+    output = corrector(prompt, do_sample=False)
+    corrected_text = output[0]["generated_text"]
+    return corrected_text.replace("Fix grammar and spelling: ", "").strip()
 
 
 
